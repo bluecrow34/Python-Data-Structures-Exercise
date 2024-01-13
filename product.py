2f5d1bdf22743cae69095cba74d86201970a8268
Cam DeSousa <134101013+bluecrow34@users.noreply.github.com>
2024-01-13 12:56:10 -0500

    Add files via upload

---
+++ product.py
@@ -0,0 +1,13 @@
+def product(a, b):
+
+    result = a *b
+
+    return result
+    """Return product of a and b.
+
+        >>> product(2, 2)
+        4
+
+        >>> product(2, -2)
+        -4
+    """
